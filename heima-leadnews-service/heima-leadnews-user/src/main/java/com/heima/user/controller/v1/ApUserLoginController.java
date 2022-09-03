189e08d66063f044c382056d957d3614cb74d180
itcast <1477853611.qq.com>
2022-09-03 17:12:13 +0800

    App登陆逻辑

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -2,6 +2,8 @@ package com.heima.user.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.user.dtos.LoginDto;
+import com.heima.user.service.ApUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/login") // 用户登录
 public class ApUserLoginController {
 
+    @Autowired
+    private ApUserService apUserService;
+
     @PostMapping("/login_auth")
     public ResponseResult login(@RequestBody LoginDto dto){
-        return null;
+        return apUserService.login(dto);
     }
 
 }
